81ab7d301fda6993d5c0ed7a1c03b2bf476dec5d
MaddieAnita <maddie@mallingmay.com>
2024-06-18 16:38:57 +0100

    allows user to increase/decrease votes on articles

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,15 +1,18 @@
 import { Fragment, useEffect, useState } from "react";
 import { getSingleArticle } from "../../api";
 import { useParams } from "react-router-dom";
+import { IoCalendarClearSharp, IoHeartSharp, IoPerson } from "react-icons/io5";
 import "../styles/single-article.css";
 import Loading from "./Loading";
 import { useNavigate } from "react-router-dom";
 import CommentList from "./CommentList";
 import ErrorComponent from "./ErrorComponent";
+import UpdateVotes from "./UpdateVotes";
 
 const SingleArticle = () => {
   const [singleArticle, setSingleArticle] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [articleVotes, setArticleVotes] = useState(0);
   const { article_id } = useParams();
   const [error, setError] = useState(null);
   let navigate = useNavigate();
@@ -19,12 +22,18 @@ const SingleArticle = () => {
     getSingleArticle(article_id)
       .then(({ article }) => {
         setSingleArticle(article);
+        setArticleVotes(article.votes);
         setIsLoading(false);
       })
       .catch((err) => {
         setError(err);
       });
   }, [article_id]);
+
+  const formatDate = (date) => {
+    const newDate = new Date(date);
+    return newDate.toLocaleDateString("en-US");
+  };
 
   if (error) {
     return <ErrorComponent error={error} />;
@@ -42,15 +51,28 @@ const SingleArticle = () => {
           <article>
             <h1>{singleArticle.title}</h1>
             <div className="article-details">
-              <p>{singleArticle.author}</p>
-              <p>{singleArticle.created_at}</p>
-              <p>{singleArticle.votes}</p>
+              <p>
+                <IoPerson /> {singleArticle.author}
+              </p>
+              <p>
+                <IoCalendarClearSharp />
+                {formatDate(singleArticle.created_at)}
+              </p>
+              <p>
+                <IoHeartSharp />
+                {articleVotes}
+              </p>
             </div>
             <img src={singleArticle.article_img_url} />
             <div className="article-body">
               <p>{singleArticle.body}</p>
             </div>
           </article>
+          <UpdateVotes
+            votes={articleVotes}
+            setVotes={setArticleVotes}
+            article_id={article_id}
+          />
           <CommentList article_id={article_id} />
         </Fragment>
       )}
