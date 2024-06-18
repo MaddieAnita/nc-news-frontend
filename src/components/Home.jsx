865c0aa2e5a7f5ecc452ca0665b67a0362389094
MaddieAnita <maddie@mallingmay.com>
2024-06-18 09:13:09 +0100

    adds loading state

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import PropTypes from "prop-types";
 import { getArticles } from "../../api";
 import ArticleList from "./ArticleList";
 import Pagination from "./Pagination";
 import PageDisplaying from "./PageDisplaying";
+import Loading from "./Loading";
 
 const Home = ({
   articles,
@@ -15,36 +16,45 @@ const Home = ({
   setPage,
 }) => {
   const [totalCount, setTotalCount] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     getArticles(page).then(({ articles, total_count }) => {
       setTotalCount(total_count);
       setArticles(articles);
+      setIsLoading(false);
     });
   }, [page]);
 
   return (
     <main>
-      <section className="container categories-list">
-        <p>CATEGORIES</p>
-      </section>
-      <section className="container featured">
-        <p>FEATURED</p>
-      </section>
-      <SearchBar />
-      <section className="container articles-container">
-        <PageDisplaying
-          articlesDisplaying={articlesDisplaying}
-          totalCount={totalCount}
-        />
-        <ArticleList articles={articles} />
-        <Pagination
-          page={page}
-          setPage={setPage}
-          totalCount={totalCount}
-          setArticlesDisplaying={setArticlesDisplaying}
-        />
-      </section>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Fragment>
+          <section className="container categories-list">
+            <p>CATEGORIES</p>
+          </section>
+          <section className="container featured">
+            <p>FEATURED</p>
+          </section>
+          <SearchBar />
+          <section className="container articles-container">
+            <PageDisplaying
+              articlesDisplaying={articlesDisplaying}
+              totalCount={totalCount}
+            />
+            <ArticleList articles={articles} />
+            <Pagination
+              page={page}
+              setPage={setPage}
+              totalCount={totalCount}
+              setArticlesDisplaying={setArticlesDisplaying}
+            />
+          </section>
+        </Fragment>
+      )}
     </main>
   );
 };
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import "../styles/loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading-box">
+      <span className="loader"></span>
+    </div>
+  );
+};
+
+export default Loading;
